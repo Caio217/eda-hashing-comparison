6f55693b9788932b572104e5c8dc750a4eb7b65a
carlossantos-05 <carlos.emanoel.barbosa.santos@ccc.ufcg.edu.br>
2026-03-06 20:35:04 -0300

    Implementando gerador de entradas e envio para a pasta dataset

--- src/main/java/hashing/HashMidSquare.java
+++ src/main/java/hashing/HashMidSquare.java
@@ -1,31 +1,23 @@
 package hashing;
 
-public class HashMidSquare<T> implements FuncaoHash<T> {
+public class HashMidSquare implements FuncaoHash {
 
     @Override
-    public int hash(T input, int capacidade) {
+    public int hash(long input, int capacidade) {
+        long quadrado = input * input;
+        
+        long meio = quadrado / 100000L; 
+        
+        return Math.abs((int) (meio % capacidade));
+    }
 
+    @Override
+    public int hash(String input, int capacidade) {
         if (input == null)
             throw new IllegalArgumentException("Chave não pode ser null");
 
-        long valor = input.hashCode();
+        long hashComoNumero = input.hashCode() & 0x7fffffff;
 
-        long quadrado = valor * valor;
-
-        quadrado = Math.abs(quadrado);
-
-        String numero = String.valueOf(quadrado);
-
-        int tamanho = numero.length();
-        int meio = tamanho / 2;
-
-        int inicio = Math.max(0, meio - 1);
-        int fim = Math.min(tamanho, meio + 2);
-
-        String parteCentral = numero.substring(inicio, fim);
-
-        int valorCentral = Integer.parseInt(parteCentral);
-
-        return valorCentral % capacidade;
+        return hash(hashComoNumero, capacidade);
     }
 }

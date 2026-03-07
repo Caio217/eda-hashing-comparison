1af46e42647e84decf15a005d81e35715ef90fae
joao.victor <joaovictormain7784@hotmail.com>
2026-03-07 18:42:29 -0300

    modificaçao da djb2

--- src/main/java/hashing/HashDJB2.java
+++ src/main/java/hashing/HashDJB2.java
@@ -1,21 +1,23 @@
-package hashing;
-
-public class HashDJB2<T> implements FuncaoHash<T> {
+public class HashDJB2 implements FuncaoHash {
 
     @Override
-    public int hash(T input, int capacidade) {
+    public int hash(String input, int capacidade) {
         if (input == null)
-            throw new IllegalArgumentException("Chave null!");
+            throw new IllegalArgumentException("Chave nao pode ser null!");
 
-        String chave = input.toString();
         long hash = 5381;
 
-        for (int i = 0; i < chave.length(); i++) {
-            hash = ((hash << 5) + hash) + chave.charAt(i);
+        for (int i = 0; i < input.length(); i++) {
+            hash = ((hash << 5) + hash) + input.charAt(i);
         }
-        //pra garantir que seja positivo
+        
         hash = hash & 0x7fffffff; 
 
         return (int) (hash % capacidade);
+    }
+
+    @Override
+    public int hash(long input, int capacidade) {
+        return hash(String.valueOf(input), capacidade);
     }
 }

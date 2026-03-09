0cdd5cccb75f5f0c03915de4943b07031b8bcf42
GuiAraujo12 <guilherme.araujo.fernandes@ccc.ufcg.edu.br>
2026-03-09 15:27:04 -0300

    Alteração no HashMultiplicacao.java

--- src/main/java/hashing/HashMultiplicacao.java
+++ src/main/java/hashing/HashMultiplicacao.java
@@ -4,7 +4,7 @@ public class HashMultiplicacao<T> implements FuncaoHash<T> {
     
     public int hash(T chave, int tamanho){
 //        if(chave  == null){
-            //return trow new IllegalArgumentException("A chave não pode ser nula");
+            //return trow new IllegalArgumentException("chave não pode ser nula");
 //        }
         int hash = chave.hashCode();
         double A = (Math.sqrt(5) - 1) / 2; 
